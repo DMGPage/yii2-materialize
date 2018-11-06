29d97c9e7e7dd7afb1181f5e5cdab3f8854024d5
Dmitrijs Reinmanis <dmgpage@gmail.com>
2018-11-06 13:18:11 +0200

    Create MaterializeFontAsset.php

---
+++ src/MaterializeFontAsset.php
@@ -0,0 +1,14 @@
+<?php
+namespace dmgpage\yii2materialize;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for the Google Icon Font files.
+ */
+class MaterializeFontAsset extends AssetBundle
+{
+    public $css = [
+        '//fonts.googleapis.com/icon?family=Material+Icons'
+    ];
+}
