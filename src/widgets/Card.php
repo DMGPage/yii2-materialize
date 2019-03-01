6ed601b9585d2141e6c0dee8c7d2d1c0863bd917
DMG <dmgpage@gmail.com>
2019-03-01 11:19:53 +0200

    / ‘src/widgets/Card.php’

--- src/widgets/Card.php
+++ src/widgets/Card.php
@@ -107,6 +107,7 @@ class Card extends Widget
 
         Html::addCssClass($this->cardOptions, ['class' => 'card']);
         Html::addCssClass($this->contentOptions, ['class' => 'card-content']);
+        echo $html;
 
         $html = Html::beginGridRow($this->options);
         $html .= Html::beginGridCol($this->colOptions);
